1ab421580176e16a74c60fc3e5b11f91a1eb2be0
lucas-vallery <92909675+lucas-vallery@users.noreply.github.com>
2022-10-19 09:50:03 +0200

    Commit de reparation

--- stm32_project_bus_et _reseaux/Core/Src/BMP280.c
+++ stm32_project_bus_et _reseaux/Core/Src/BMP280.c
@@ -9,10 +9,11 @@
 
 uint8_t BMP280_getId(){
 	uint8_t id;
-
-	if(HAL_OK == HAL_I2C_Master_Transmit(&hi2c1, I2C_ADD, ID, 1, HAL_MAX_DELAY)){
+	uint8_t idRegister = (uint8_t)ID;
+	if(HAL_OK == HAL_I2C_Master_Transmit(&hi2c1, I2C_ADD, idRegister, 1, HAL_MAX_DELAY)){
+		printf("là");
 		if (HAL_OK == HAL_I2C_Master_Receive(&hi2c1, I2C_ADD, &id, 1, HAL_MAX_DELAY)){
-
+			printf("ici");
 		}
 	}
 
