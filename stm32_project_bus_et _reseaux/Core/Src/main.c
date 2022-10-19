9dc2f5104c9c7c09c45474e41f93d03c96bf0e24
lucas-vallery <92909675+lucas-vallery@users.noreply.github.com>
2022-10-19 08:46:09 +0200

    printf commit
    Redirecting the printf to the usb

--- stm32_project_bus_et _reseaux/Core/Src/main.c
+++ stm32_project_bus_et _reseaux/Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <stdio.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -100,7 +100,8 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+	  printf("hello world!\r\n");
+	  HAL_Delay(1000);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
