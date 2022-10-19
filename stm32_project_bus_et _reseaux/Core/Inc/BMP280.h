317f213f4e0d55d23533f276233f99fbb8a20155
lucas-vallery <92909675+lucas-vallery@users.noreply.github.com>
2022-10-19 09:57:59 +0200

    test

--- stm32_project_bus_et _reseaux/Core/Inc/BMP280.h
+++ stm32_project_bus_et _reseaux/Core/Inc/BMP280.h
@@ -3,23 +3,26 @@
  *
  *  Created on: Oct 19, 2022
  *      Author: jean-francoiscastellani
- */
+*/
 
 #ifndef INC_BMP280_H_
 #define INC_BMP280_H_
 
 #include "main.h"
 
-#define ID 0xD0
-#define RESET 0xE0
-#define I2C_ADD 0x77
-#define CTRL_MEAS 0xF4
-#define TEMP_XLSB 0xFC
-#define TEMP_LSB 0xFB
-#define TEMP_MSB 0xFA
-#define PRESS_XLSB 0xF9
-#define PRESS_LSB 0xF8
-#define PRESS_MSB 0xF7
+#define TEMP_XLSB 	0xFC
+#define TEMP_LSB	0xFB
+#define TEMP_MSB	0xFA
+#define PRESS_XLSB	0xF9
+#define PRESS_LSB	0xF8
+#define PRESS_MSB 	0xF7
+#define CONFIG		0xF5
+#define CTRL_MEAS	0xF4
+#define STATUS 		0xF3
+#define RESET		0xE0
+#define ID			0xE0
+#define CALIB_START	0x88
+#define I2C_ADD 	0x68
 
 extern I2C_HandleTypeDef hi2c1;
 
